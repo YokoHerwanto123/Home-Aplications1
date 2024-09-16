2fe76535b66f6d72f4f72ab8dae3a98a3aa5eda9
YokoHerwanto123 <yokoherwanto205@gmail.com>
2024-09-16 22:46:15 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,81 @@
+function setDapurLed() {
+    ledDapur.style.backgroundColor = "red";
+    dapurLedImage.src = "./assets/lamp-on.png";
+}
+
+function setTerasLed() {
+    ledTeras.style.backgroundColor = "red";
+    terasLedImage.src = "./assets/lamp-on.png";
+}
+
+function setKamarLed() {
+    ledKamar.style.backgroundColor = "red";
+    kamarLedImage.src = "./assets/lamp-on.png";
+}
+
+function setTamuLed() {
+    ledTamu.style.backgroundColor = "red";
+    tamuLedImage.src = "./assets/lamp-on.png";
+}
+
+function setToiletLed() {
+    ledToilet.style.backgroundColor = "red";
+    toiletLedImage.src = "./assets/lamp-on.png";
+}
+
+function setKamartidurLed() {
+    kamarTidur.style.backgroundColor = "red";
+    kamartidurLedImage.src = "./assets/lamp-on.png";
+}
+
+
+function setFanone() {
+    fanone.style.backgroundColor = "red";
+    FanImageone.src = "./assets/fan-on.png";
+}
+
+function setFantwo() {
+    fantwo.style.backgroundColor = "red";
+    FanImagetwo.src = "./assets/fan-on.png";
+}
+
+
+/*function setMesinCuciOne() {
+    WachingmachineOne.style.backgroundColor = "red";
+    wachingmachinedalamone-submit.src = "./assets/mesincuci-on.png";
+}
+
+function setMesinCuciTwo() {
+    mesincuciTwo.style.backgroundColor = "red";
+    mesincucitwoImage.src = "./assets/mesincuci-on.png";
+}*/
+
+
+function setValveOne() {
+    valveOne.style.backgroundColor = "red";
+    valveoneImage.src = "./assets/valve-on.png";
+}
+
+function setValveTwo() {
+    valveTwo.style.backgroundColor = "red";
+    valvetwoImage.src = "./assets/valve-on.png";
+}
+
+function setValveThree() {
+    valveThree.style.backgroundColor = "red";
+    valvethreeImage.src = "./assets/valve-on.png";
+}
+
+function setValveFour() {
+    valveFour.style.backgroundColor = "red";
+    valvefourImage.src = "./assets/valve-on.png";
+}
+
+
+function setMesinCuciOne() {
+    window.location.href = "wachingmachineone.html";
+}
+
+function setMesinCuciTwo() {
+    window.location.href = "wachingmachinetwo.html";
+}
